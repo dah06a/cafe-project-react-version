299152c4b13aa412fc12cff0cf89594b0847587c
David Henry <dah06a@acu.edu>
2021-02-06 10:12:15 -0600

    Initial commit to start project.

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <Row>
+          <Col>1</Col>
+          <Col>2</Col>
+          <Col>3</Col>
+          <Col>4</Col>
+          <Col>5</Col>
+          <Col>6</Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+
+const MainNavbar = (props) => <Navbar collapseOnSelect expand="md" bg="dark">
+    <Navbar></Navbar>
+</Navbar>
+
+export default MainNavbar;
