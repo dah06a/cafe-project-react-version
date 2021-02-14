890e947da62c3cdcb910c06f350783108d11ab55
David Henry <dah06a@acu.edu>
2021-02-14 15:55:42 -0600

    Fixed landing image sizing.

--- src/components/home/HomeLanding.js
+++ src/components/home/HomeLanding.js
@@ -5,11 +5,11 @@ import FullTablesImg from '../../images/full-tables.jpg';
 const HomeLanding = (props) => {
     return(
         <Container fluid className="home-landing text-center">
-            <Row className="h-75">
-                <Col md={"6"} style={{marginTop: "15%"}}>
+            <Row className="h-100">
+                <Col md={6} style={{marginTop: "20%"}}>
                     <h1 className="theme-font" style={{color: "var(--theme-primary)"}}>The Cafe Where You Can Play</h1>
                 </Col>
-                <Col md={"6"} className="my-auto">
+                <Col md={6} className="my-auto">
                     <Image src={FullTablesImg} alt="Cafe" className="home-landing-img" roundedCircle />
                 </Col>
             </Row>
